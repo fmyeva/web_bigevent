4852529851e3b4d7158d1059fa74b473c080d0ec
fmyeva <3109228691@qq.com>
2022-04-28 09:46:37 +0800

    完成了主页开发

--- js/baseAPI.js
+++ js/baseAPI.js
@@ -2,4 +2,19 @@
 //ajaxPrefilter函数可以拿到ajax提供的配置对象
 $.ajaxPrefilter(function(options){
     options.url = 'http://www.liulongbin.top:3007' + options.url
+
+    if(options.url.indexOf('/my/') !== -1 ){
+        //统一为有权限的接口设置headers请求头
+    options.headers = {Authorization:localStorage.getItem('token')||''}
+    }
+            //全局统一挂载
+            //无论成功还是失败最终都会调用complete函数
+            //在回调函数中，可以使用res.responseJSON拿到服务器响应回来的数据
+     options.complete = function(res){
+                if(res.responseJSON.status === 1 && res.responseJSON.message === '身份验证失败！')
+                //强制清空token，跳转登录页面
+                localStorage.removeItem('token')
+                location.href = 'login.html'
+            }
+    
 })
